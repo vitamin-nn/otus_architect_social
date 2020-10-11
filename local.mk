c427d809c9496ff682d4d8f6e4a1c53f3d3430af
Vitaly Dudov <dudovv@mail.ru>
2020-10-11 21:25:30 +0300

    heroku migrations

--- local.mk
+++ local.mk
@@ -1,1 +1,10 @@
+ifneq (,$(wildcard ./deployments/env/prod.database.env))
+    include ./deployments/env/prod.database.env
+    export
+endif
 
+migrate-heroku:
+	goose -dir migrate/migrations mysql "${MYSQL_USER}:${MYSQL_PASSWORD}@tcp(${MYSQL_DB_HOST})/${MYSQL_DATABASE}?parseTime=true" up
+
+migrate:
+	goose -dir migrate/migrations mysql "otus_social:otus_social_passwd@tcp(0.0.0.0:3306)/otus_social?parseTime=true" up
